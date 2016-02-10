6f0290f0535f9bb919d04b2df7e79fa6f74975a2
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-10 21:08:19 +0700

    update layout css

--- application/views/layout/css.php
+++ application/views/layout/css.php
@@ -1,1 +1,8 @@
 <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url(); ?>assets/css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+    body {
+        min-height: 2000px;
+        padding-top: 70px;
+    }
+</style>
