a2e45c49dc7edb871891cc5f78fe190e9a187a27
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-10 22:14:59 +0700

    update kursus view untuk tambah kursus

--- application/views/KursusView.php
+++ application/views/KursusView.php
@@ -35,6 +35,10 @@
 
                         <p></p>
 
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#myModal">Tambah</button>
+
+                        <p></p>
+
                         <table id="tabel1" class="table table-bordered table-hover table-responsive table-striped">
                             <thead>
                                 <tr>
@@ -82,6 +86,32 @@
                                 <?php } ?>
                             </tbody>
                         </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Tambah Jadwal Kursus</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form action="<?php echo base_url(); ?>index.php/KursusController/simpanJadwalKursus" method="post">
+                            <div class="form-group">
+                                <label>Kelas</label>
+                                <input type="text" name="kelas" class="form-control" placeholder="Masukkan Kelas">
+                            </div>
+                            <div class="form-group">
+                                <label>Modul</label>
+                                <input type="text" name="modul" class="form-control" placeholder="Masukkan Modul">
+                            </div>
+                            <button type="button" class="btn btn-warning" data-dismiss="modal">Batal</button>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </form>
                     </div>
                 </div>
             </div>
