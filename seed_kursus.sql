4797b2ed8030b943c341c0040571f319893f3378
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-10 14:06:06 +0700

    update query seed kursus

--- seed_kursus.sql
+++ seed_kursus.sql
@@ -86,31 +86,57 @@ VALUES
 
 -- jadwal sesi kursus
 
+set @uuid_jadwal_sesi_kursus1 = uuid();
+set @uuid_jadwal_sesi_kursus2 = uuid();
+set @uuid_jadwal_sesi_kursus3 = uuid();
+set @uuid_jadwal_sesi_kursus4 = uuid();
+set @uuid_jadwal_sesi_kursus5 = uuid();
+set @uuid_jadwal_sesi_kursus6 = uuid();
+set @uuid_jadwal_sesi_kursus7 = uuid();
+set @uuid_jadwal_sesi_kursus8 = uuid();
+set @uuid_jadwal_sesi_kursus9 = uuid();
+set @uuid_jadwal_sesi_kursus10 = uuid();
+set @uuid_jadwal_sesi_kursus11 = uuid();
+set @uuid_jadwal_sesi_kursus12 = uuid();
+set @uuid_jadwal_sesi_kursus13 = uuid();
+set @uuid_jadwal_sesi_kursus14 = uuid();
+set @uuid_jadwal_sesi_kursus15 = uuid();
+set @uuid_jadwal_sesi_kursus16 = uuid();
+set @uuid_jadwal_sesi_kursus17 = uuid();
+set @uuid_jadwal_sesi_kursus18 = uuid();
+set @uuid_jadwal_sesi_kursus19 = uuid();
+set @uuid_jadwal_sesi_kursus20 = uuid();
+set @uuid_jadwal_sesi_kursus21 = uuid();
+set @uuid_jadwal_sesi_kursus22 = uuid();
+set @uuid_jadwal_sesi_kursus23 = uuid();
+set @uuid_jadwal_sesi_kursus24 = uuid();
+set @uuid_jadwal_sesi_kursus25 = uuid();
+
 INSERT INTO `penjadwalan`.`sesi_kursus`
 (`id_sesi_kursus`,`sesi`,`kelas`,`modul`,`id_ruang_kursus`)
 VALUES
-(uuid(),0,'4IA01','java',@uuid_jadwal_ruang_kursus1),
-(uuid(),1,'4IA02','c++',@uuid_jadwal_ruang_kursus1),
-(uuid(),2,null,null,@uuid_jadwal_ruang_kursus1),
-(uuid(),3,null,null,@uuid_jadwal_ruang_kursus1),
-(uuid(),4,'4IA01','asp',@uuid_jadwal_ruang_kursus1),
-(uuid(),0,null, null,@uuid_jadwal_ruang_kursus2),
-(uuid(),1,'4IA02','jsp',@uuid_jadwal_ruang_kursus2),
-(uuid(),2,'4IA01','myob',@uuid_jadwal_ruang_kursus2),
-(uuid(),3,'4IA03','pweb',@uuid_jadwal_ruang_kursus2),
-(uuid(),4,null, null,@uuid_jadwal_ruang_kursus2),
-(uuid(),0,'4IA04','java',@uuid_jadwal_ruang_kursus3),
-(uuid(),1,'4IA05','c++',@uuid_jadwal_ruang_kursus3),
-(uuid(),2,'4IA04','jarkom',@uuid_jadwal_ruang_kursus3),
-(uuid(),3,'4IA02','jarkomlan',@uuid_jadwal_ruang_kursus3),
-(uuid(),4,null,null,@uuid_jadwal_ruang_kursus3),
-(uuid(),0,null,null,@uuid_jadwal_ruang_kursus4),
-(uuid(),1,null,null,@uuid_jadwal_ruang_kursus4),
-(uuid(),2,'4IA011','java',@uuid_jadwal_ruang_kursus4),
-(uuid(),3,'4IA016','c++',@uuid_jadwal_ruang_kursus4),
-(uuid(),4,'4IA013','java',@uuid_jadwal_ruang_kursus4),
-(uuid(),0,null,null,@uuid_jadwal_ruang_kursus5),
-(uuid(),1,null,null,@uuid_jadwal_ruang_kursus5),
-(uuid(),2,'4IA03','java',@uuid_jadwal_ruang_kursus5),
-(uuid(),3,'4IA06','c++',@uuid_jadwal_ruang_kursus5),
-(uuid(),4,'4IA010','java',@uuid_jadwal_ruang_kursus5);
+(@uuid_jadwal_sesi_kursus1,0,'4IA01','java',@uuid_jadwal_ruang_kursus1),
+(@uuid_jadwal_sesi_kursus2,1,'4IA02','c++',@uuid_jadwal_ruang_kursus1),
+(@uuid_jadwal_sesi_kursus3,2,null,null,@uuid_jadwal_ruang_kursus1),
+(@uuid_jadwal_sesi_kursus4,3,null,null,@uuid_jadwal_ruang_kursus1),
+(@uuid_jadwal_sesi_kursus5,4,'4IA01','asp',@uuid_jadwal_ruang_kursus1),
+(@uuid_jadwal_sesi_kursus6,0,null, null,@uuid_jadwal_ruang_kursus2),
+(@uuid_jadwal_sesi_kursus7,1,'4IA02','jsp',@uuid_jadwal_ruang_kursus2),
+(@uuid_jadwal_sesi_kursus8,2,'4IA01','myob',@uuid_jadwal_ruang_kursus2),
+(@uuid_jadwal_sesi_kursus9,3,'4IA03','pweb',@uuid_jadwal_ruang_kursus2),
+(@uuid_jadwal_sesi_kursus10,4,null, null,@uuid_jadwal_ruang_kursus2),
+(@uuid_jadwal_sesi_kursus11,0,'4IA04','java',@uuid_jadwal_ruang_kursus3),
+(@uuid_jadwal_sesi_kursus12,1,'4IA05','c++',@uuid_jadwal_ruang_kursus3),
+(@uuid_jadwal_sesi_kursus13,2,'4IA04','jarkom',@uuid_jadwal_ruang_kursus3),
+(@uuid_jadwal_sesi_kursus14,3,'4IA02','jarkomlan',@uuid_jadwal_ruang_kursus3),
+(@uuid_jadwal_sesi_kursus15,4,null,null,@uuid_jadwal_ruang_kursus3),
+(@uuid_jadwal_sesi_kursus16,0,null,null,@uuid_jadwal_ruang_kursus4),
+(@uuid_jadwal_sesi_kursus17,1,null,null,@uuid_jadwal_ruang_kursus4),
+(@uuid_jadwal_sesi_kursus18,2,'4IA011','java',@uuid_jadwal_ruang_kursus4),
+(@uuid_jadwal_sesi_kursus19,3,'4IA016','c++',@uuid_jadwal_ruang_kursus4),
+(@uuid_jadwal_sesi_kursus20,4,'4IA013','java',@uuid_jadwal_ruang_kursus4),
+(@uuid_jadwal_sesi_kursus21,0,null,null,@uuid_jadwal_ruang_kursus5),
+(@uuid_jadwal_sesi_kursus22,1,null,null,@uuid_jadwal_ruang_kursus5),
+(@uuid_jadwal_sesi_kursus23,2,'4IA03','java',@uuid_jadwal_ruang_kursus5),
+(@uuid_jadwal_sesi_kursus24,3,'4IA06','c++',@uuid_jadwal_ruang_kursus5),
+(@uuid_jadwal_sesi_kursus25,4,'4IA010','java',@uuid_jadwal_ruang_kursus5);
