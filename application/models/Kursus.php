6e9f6d3cc17eeae9b22aa5b6ecfe1adbb2e04bc9
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-10 15:25:58 +0700

    tambah model kursus

---
+++ application/models/Kursus.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * Since Feb 10, 2016
+ * Time 2:30:32 PM
+ * Encoding UTF-8
+ * Project Aplikasi-Penjadwalan
+ * Package Expression package is undefined on line 12, column 14 in Templates/Scripting/PHPClass.php.
+ * 
+ */
+class Kursus extends CI_Model {
+
+    public function ruangKursusKosong() {
+        $this->db->select('id_sesi_kursus');
+        $this->db->select('sesi');
+        return $this->db->get('cari_ruang_sesi_kursus_kosong')->result();
+    }
+
+    public function tambahJadwalKursus($dataKursus, $id) {
+        $data = array(
+            'kelas' => $dataKursus['kelas'],
+            'modul' => $dataKursus['modul']
+        );
+        $this->db->where('id_sesi_kursus', $id);
+        $this->db->update('sesi_kursus', $data);
+    }
+
+}
