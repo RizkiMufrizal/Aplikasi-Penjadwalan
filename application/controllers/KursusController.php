bf7c6de2cc8bce0b00ca5ef879d4b1d8139be418
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-10 22:52:14 +0700

    tambah upload jadwal kursus

--- application/controllers/KursusController.php
+++ application/controllers/KursusController.php
@@ -16,6 +16,7 @@ class KursusController extends CI_Controller {
         parent::__construct();
         $this->load->model('Kursus');
         $this->load->model('Kuliah');
+        $this->load->library('CSVReader');
     }
 
     public function simpanJadwalKursus() {
@@ -56,6 +57,39 @@ class KursusController extends CI_Controller {
         $data['dataKursus'] = $this->Kursus->getAllJadwalKursus();
         $data['dataKursusKosong'] = $this->Kursus->getAllJadwalKursusKosong();
         $this->load->view('KursusView', $data);
+    }
+
+    public function uploadJadwalKursus() {
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'csv';
+        $config['max_size'] = '1000';
+
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload()) {
+            $data['error'] = $this->upload->display_errors();
+
+            $this->load->view('csvindex', $data);
+        } else {
+            $file_data = $this->upload->data();
+            $file_path = './uploads/' . $file_data['file_name'];
+
+            $result = $this->csvreader->parse_file($file_path);
+            foreach ($result as $row) {
+                $id = $this->Kursus->getKursusByRuang($row['ruang'])[0]->id_ruang_kursus;
+
+                $val = array(
+                    'sesi' => $row['sesi'],
+                    'modul' => empty($row['modul']) ? NULL : $row['modul'],
+                    'kelas' => empty($row['kelas']) ? NULL : $row['kelas'],
+                    'id_ruang_kursus' => $id
+                );
+
+                $this->Kursus->simpanJadwalKursus($val);
+            }
+
+            redirect('kursus');
+        }
     }
 
 }
