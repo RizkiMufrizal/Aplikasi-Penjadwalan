615ebb3a3f931017b2e153f69039ee2c4e9f26e9
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-10 21:09:22 +0700

    tambah kursus view

---
+++ application/views/KursusView.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+
+ Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ Since Feb 10, 2016
+ Time 6:20:45 PM
+ Encoding UTF-8
+ Project Aplikasi-Penjadwalan
+ Package Expression package is undefined on line 9, column 12 in Templates/Scripting/EmptyPHPWebPage.php.
+  
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aplikasi Penjadwalan</title>
+
+        <?php $this->load->view('layout/css') ?>
+
+    </head>
+    <body>
+
+        <?php $this->load->view('layout/header') ?>
+
+        <div class="container">
+
+            <div class="jumbotron text-center">
+                <h1>Selamat Datang</h1>
+                <p>Aplikasi penjadwalan berfungsi untuk mengatur jadwal kursus</p>
+            </div>
+
+        </div>
+
+        <?php $this->load->view('layout/js') ?>
+
+    </body>
+</html>
