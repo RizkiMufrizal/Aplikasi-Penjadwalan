8d4abaf9dfe42206b73aad07c7486c1330cc72e9
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-10 21:08:29 +0700

    update layout js

--- application/views/layout/js.php
+++ application/views/layout/js.php
@@ -1,2 +1,10 @@
 <script src="<?php echo base_url(); ?>assets/js/jquery-1.12.0.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>assets/js/dataTables.bootstrap.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#tabel1').DataTable();
+        $('#tabel2').DataTable();
+    });
+</script>
