0c0f527f71e1c36df9ba99cf7404378c644d5260
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-25 20:46:10 +0700

    tambah query model kursus

--- application/models/Kursus.php
+++ application/models/Kursus.php
@@ -8,49 +8,81 @@
  * Encoding UTF-8
  * Project Aplikasi-Penjadwalan
  * Package Expression package is undefined on line 12, column 14 in Templates/Scripting/PHPClass.php.
- * 
+ *
  */
-class Kursus extends CI_Model {
+class Kursus extends CI_Model
+{
 
-    public function ruangKursusKosong() {
+    public function ruangKursusKosong()
+    {
         $this->db->select('id_sesi_kursus');
         $this->db->select('sesi');
         return $this->db->get('cari_ruang_sesi_kursus_kosong')->result();
     }
 
-    public function getAllJadwalKursus() {
+    public function getAllJadwalKursus()
+    {
         return $this->db->get('query_jadwal_kursus')->result();
     }
 
-    public function getAllJadwalKursusKosong() {
+    public function getAllJadwalKursusKosong()
+    {
         return $this->db->get('cari_ruang_sesi_kursus_kosong')->result();
     }
 
-    public function getKursusByHari($hari) {
+    public function getKursusByHari($hari)
+    {
         $this->db->select('id_jadwal_kursus');
         $this->db->where('hari', $hari);
         return $this->db->get('jadwal_kursus')->result();
     }
 
-    public function getRuangKursusByRuangAndId($ruang, $id_jadwal_kursus) {
+    public function getRuangKursusByRuangAndId($ruang, $id_jadwal_kursus)
+    {
         $this->db->select('id_ruang_kursus');
         $this->db->where('ruang', $ruang);
         $this->db->where('id_jadwal_kursus', $id_jadwal_kursus);
         return $this->db->get('ruang_kursus')->result();
     }
 
-    public function simpanJadwalKursus($dataKursus) {
+    public function simpanJadwalKursus($dataKursus)
+    {
         $val = array(
-            'id_sesi_kursus' => $this->uuid->v4(),
-            'sesi' => $dataKursus['sesi'],
-            'modul' => $dataKursus['modul'],
-            'id_ruang_kursus' => $dataKursus['id_ruang_kursus']
+            'id_sesi_kursus'  => $this->uuid->v4(),
+            'sesi'            => $dataKursus['sesi'],
+            'modul'           => $dataKursus['modul'],
+            'id_ruang_kursus' => $dataKursus['id_ruang_kursus'],
         );
         $this->db->insert('sesi_kursus', $val);
     }
 
-    public function hapusAllDataKursus() {
+    public function hapusAllDataKursus()
+    {
         $this->db->truncate('sesi_kursus');
+    }
+
+    public function cari_nama_kelas_kursus_kosong($modul)
+    {
+        $this->db->where('modul', $modul);
+        return $this->db->get('cari_nama_kelas_kursus_kosong')->result();
+    }
+
+    public function simpanAllJadwalKursus($dataKursus)
+    {
+        $val = array(
+            'id_all_jadwal_kursus' => $this->uuid->v4(),
+            'ruang'                => $dataKursus['ruang'],
+            'hari'                 => $dataKursus['hari'],
+            'sesi'                 => $dataKursus['sesi'],
+            'modul'                => $dataKursus['modul'],
+            'jumlah'               => $dataKursus['jumlah'],
+        );
+        $this->db->insert('tb_all_jadwal_kursus', $val);
+    }
+
+    public function getAJadwalKursus()
+    {
+        return $this->db->get('tb_all_jadwal_kursus')->result();
     }
 
 }
