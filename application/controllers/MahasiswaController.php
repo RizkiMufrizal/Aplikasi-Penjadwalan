9e148730a963d830c3d2810b75eb87f79e7c2d44
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-25 20:47:04 +0700

    format

--- application/controllers/MahasiswaController.php
+++ application/controllers/MahasiswaController.php
@@ -8,25 +8,29 @@
  * Encoding UTF-8
  * Project Aplikasi-Penjadwalan
  * Package Expression package is undefined on line 12, column 14 in Templates/Scripting/PHPClass.php.
- * 
+ *
  */
-class MahasiswaController extends CI_Controller {
+class MahasiswaController extends CI_Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         $this->load->model('Mahasiswa');
         $this->load->library('CSVReader');
     }
 
-    public function pageMahasiswa() {
+    public function pageMahasiswa()
+    {
         $data['dataMahasiswa'] = $this->Mahasiswa->getMahasiswa();
         $this->load->view('MahasiswaView', $data);
     }
 
-    public function uploadMahasiswa() {
-        $config['upload_path'] = './uploads/';
+    public function uploadMahasiswa()
+    {
+        $config['upload_path']   = './uploads/';
         $config['allowed_types'] = 'csv';
-        $config['max_size'] = '1000';
+        $config['max_size']      = '1000';
 
         $this->load->library('upload', $config);
 
@@ -41,10 +45,10 @@ class MahasiswaController extends CI_Controller {
             $result = $this->csvreader->parse_file($file_path);
             foreach ($result as $row) {
                 $val = array(
-                    'npm' => $row['npm'],
-                    'nama' => $row['nama'],
+                    'npm'   => $row['npm'],
+                    'nama'  => $row['nama'],
                     'kelas' => $row['kelas'],
-                    'modul' => $row['modul']
+                    'modul' => $row['modul'],
                 );
                 $this->Mahasiswa->simpanMahasiswa($val);
             }
--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -8,15 +8,18 @@
  * Encoding UTF-8
  * Project Aplikasi-Penjadwalan
  * Package Expression package is undefined on line 12, column 14 in Templates/Scripting/PHPClass.php.
- * 
+ *
  */
-class PageController extends CI_Controller {
+class PageController extends CI_Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function index() {
+    public function index()
+    {
         $this->load->view('IndexView');
     }
 
